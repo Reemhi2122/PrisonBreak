ed464d1755469dd1fda99348b777fc34e0104f10
Stan Vogels <stan.vogels@yunify.nl>
2019-02-18 21:38:27 +0100

    Added some classes

--- Assets/Scenes/Game/Scripts/Ai/Prisoner.cs
+++ Assets/Scenes/Game/Scripts/Ai/Prisoner.cs
@@ -7,8 +7,16 @@ public class Prisoner : Human
 
     private int cellNum;
 
-    public Prisoner()
+    public Prisoner(uint weight, uint health, uint armor, int cellNum) : base(weight, health, armor)
     {
+        this.cellNum = cellNum;
+    }
 
+    public int GetCellNum(){
+        return cellNum;
+    }
+
+    public void SetCellNum(int cellNum){
+        this.cellNum = cellNum;
     }
 }
